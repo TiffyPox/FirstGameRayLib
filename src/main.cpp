b9879bc3a483252b2852f7329b8738604b97869f
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-10-29 15:13:04 -0400

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,35 @@ int main(void)
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
+    const char menuText[] = "Tiff's Game!";
+    const float textSize = 50.0f;
+    
+    InitWindow(screenWidth, screenHeight, "Hello Github");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitAudioDevice();
+
+    Music music = LoadMusicStream("../res/menuMusic.mp3");
+
+    Font font = LoadFont("../res/menuFont.otf");
 
     SetTargetFPS(60);
 
+    PlayMusicStream(music);
+    
+    int textPos = screenWidth / 2 - (MeasureText(menuText, textSize) / 2);
+
     while (!WindowShouldClose())
     {
+        UpdateMusicStream(music);
+
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(PINK);
 
-        DrawText("Tiff's Game", 190, 200, 20, LIGHTGRAY);
+        DrawText(menuText, textPos, screenHeight / 2 - font.baseSize, textSize, DARKPURPLE);
 
         EndDrawing();
     }
+
+    CloseWindow();
 }
