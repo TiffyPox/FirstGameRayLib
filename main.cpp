13ac7fdcf3c8a8e40cb4019a4969a70b5cf067ac
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-10-29 11:53:29 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Tiff's Game", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+}
